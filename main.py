78f2d8d7bf9471aed206771efb0e47f1b9a40ea6
RoberCF <robertocastanofernandez@gmail.com>
2026-03-09 10:41:57 +0100

    Update main.py
    He incluído mi propio código

--- main.py
+++ main.py
@@ -1,38 +1,51 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
+import datetime as dt
+import pandas as pd
 import smtplib
+from email.message import EmailMessage
+from random import choice
 import os
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+# Comprobamos la fecha actual y la guardamos
+fecha = dt.datetime.now()
+fecha_hoy = (fecha.month, fecha.day)
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+# Comprobamos si la fecha actual coincide con el cumpleaños de alguna persona en el listado csv
+listado_personas = pd.read_csv("lista_cumpleaños.csv")
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+diccionario = {(datos_fila.mes, datos_fila.dia): datos_fila for (index, datos_fila) in listado_personas.iterrows()}
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+if fecha_hoy in diccionario:
+    datos_cumpleañero = diccionario[fecha_hoy]
+
+    # Abrimos una carta al azar, y generamos otra nueva temporal con el nombre del cumpleañero
+    cartas = ["./plantillas_correo/carta_1.txt", "./plantillas_correo/carta_2.txt", "./plantillas_correo/carta_3.txt"]
+    carta_al_azar = choice(cartas)
+    with open(carta_al_azar, mode="r") as carta:
+        carta_original = carta.read()
+
+    # Guardamos una copia modificaca de la original con el nombre asignado
+    carta_personal = carta_original.replace("[NOMBRE]", datos_cumpleañero["nombre"])
+
+    # Enviamos el correo de felicitación a la persona indicada
+    mi_correo = "pythonpruebascurso@gmail.com"
+    mi_contraseña = "pine wpwn vkpf rebz"
+
+    # Datos contenido del correo
+    mensaje = EmailMessage()
+    mensaje["Subject"] = "¡Feliz Cumpleaños!"
+    mensaje["From"] = mi_correo
+    mensaje["To"] = datos_cumpleañero["email"]
+    mensaje.set_content(carta_personal)
+
+    # Abrimos, como si fuera un archivo, la conexión con nuestro servidor de correo
+    with smtplib.SMTP("smtp.gmail.com", port=587) as conexion:
+        conexion.starttls() # Protejemos encriptando nuestro correo
+        conexion.login(user=mi_correo, password=mi_contraseña)
+        conexion.send_message(mensaje)
+
+
+
+
+
+
+
